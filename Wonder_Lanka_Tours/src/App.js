78961681e89a893fe25f802a82074cda0f0236b7
SKShehan <shehansilva2013@gmail.com>
2021-09-11 00:29:10 +0530

    Update App.js
    ..

--- Wonder_Lanka_Tours/src/App.js
+++ Wonder_Lanka_Tours/src/App.js
@@ -1,51 +1,37 @@
 import React from "react";
-import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
-
-import IndexHeader from "components/Headers/IndexHeader";
-import IndexNavbar from "components/Navbars/IndexNavbar";
+import { useState } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
 import LandingPage from "views/examples/LandingPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
-
-import AddItinerary from "views/AddItinerary";
-import ViewItineraries from "views/viewItineraries";
-import EditItinerary from "views/EditItinerary";
-
-import EditGuide from "views/EditGuide";
-import AddGuide from "views/AddGuide";
-import ViewGuides from "views/ViewGuides";
-import GuideManagement from "views/GuideManagement";
-import BookingManagement from "views/BookingManagement";
-
-import AddHotel from "views/AddHotel";
-import HotelDetails from "views/HotelDetails";
-import EditHotel from "views/EditHotel";
-import HotelHome from "views/HotelHome";
-import Booktable from "views/booktable";
-import Bookingdetails from "views/bookingdetails";
-import Editbookings from "views/editbookings";
-import Cancelbookingform from "views/Cancelbookingform";
-
-import DriverUpdate from "views/DriverUpdate";
-import AddDriver from "views/AddDriver";
-import DriverDetails from "views/DriverDetails";
-
-import MyComplaints from "views/MyComplaints";
-import MyFeedbacks from "views/MyFeedbacks";
-
-
-
+import BookTour from "views/BookTour";
+import MyTours from "views/MyTours";
+import ViewTour from "views/ViewTour";
+import UserProfile from "views/UserProfile";
+import EditProfile from "views/EditProfile";
+import ChangePassword from "views/ChangePassword";
+import Unregister from "views/Unregister";
+import Dashboard from "views/Dashboard";
+import BookingReport from "views/BookingReport";
 
 function App() {
-
+  const [user, setuser] = useState({
+    username: "johncena",
+    password: "pass123",
+    fullName: "James Anderson",
+    country: "United Kingdom",
+    nic: "000000000",
+    mobileNo: "+94772665133",
+    email: "james@gmail.com",
+    dob: "1969-04-01",
+  });
   return (
     <BrowserRouter>
-      
       <Switch>
-      <Route path="/index" render={(props) => <Index {...props} />} />
+        <Route path="/index" render={(props) => <Index {...props} />} />
         <Route
           path="/nucleo-icons"
           render={(props) => <NucleoIcons {...props} />}
@@ -62,130 +48,33 @@ function App() {
           path="/register-page"
           render={(props) => <RegisterPage {...props} />}
         />
-
-        
-        <Route //Setting path for Add Itinerary view
-          path="/add-itinerary"
-          exact
-        >
-          <AddItinerary />
+        <Route path="/book-tour" exact>
+          <BookTour user={user}></BookTour>
         </Route>
-
-        <Route //Setting path for Edit Itinerary view
-          path="/view-itineraries"
-          exact
-        >
-          <ViewItineraries />
+        <Route path="/my-tours" exact>
+          <MyTours user={user}></MyTours>
         </Route>
-
-        <Route path="/edit-itinerary/:id" exact>
-          <EditItinerary />
+        <Route path="/view-tour" exact>
+          <ViewTour user={user}></ViewTour>
         </Route>
-        <Route path= "/edit-guide/:id" exact>
-          <EditGuide />
+        <Route path="/edit-profile" exact>
+          <EditProfile user={user}></EditProfile>
         </Route>
-        <Route path="/add-guide" exact>
-          <AddGuide />
+        <Route path="/change-password" exact>
+          <ChangePassword user={user}></ChangePassword>
         </Route>
-        <Route path="/view-guides" exact>
-          <ViewGuides />
+        <Route path="/user-profile" exact>
+          <UserProfile user={user}></UserProfile>
         </Route>
-        <Route path="/guide-management" exact>
-          <GuideManagement />
+        <Route path="/unregister" exact>
+          <Unregister user={user}></Unregister>
         </Route>
-        <Route path="/booking-management" exact>
-          < BookingManagement/>
+        <Route path="/user-dashboard" exact>
+          <Dashboard user={user}></Dashboard>
         </Route>
-
-
-      
-        <Route path="/booktable" exact>
-          <Booktable />
+        <Route path="/booking-report" exact>
+          <BookingReport user={user}></BookingReport>
         </Route>
-        <Route path="/edit-bookingmanagement/:id"  component={Editbookings}>
-        </Route>
-   
-      
-        <Route path="/add-cancelbooking/:id" component={Cancelbookingform} >
-        </Route>
-
-       
-        <Route path="/edit-driver/:id" component={DriverUpdate} />
-        <Route path ="/Add-Driver" component={AddDriver}/>
-        <Route path ="/View-Driver" component={DriverDetails}/>
-
-
-        
-        <Route //Setting path for hotels
-          path="/" exact >
-          <HotelHome />
-        </Route>
-
-        <Route path="/add" exact >
-          <AddHotel />
-        </Route>
-
-        <Route path= "/edit-guide/:id" exact>
-          <EditGuide />
-        </Route>
-        <Route path="/add-guide" exact>
-          <AddGuide />
-        </Route>
-        <Route path="/view-guides" exact>
-          <ViewGuides />
-        </Route>
-        <Route path="/guide-management" exact>
-          <GuideManagement />
-        </Route>
-        <Route path="/booking-management" exact>
-          < BookingManagement/>
-        </Route>
-
-
-      
-        <Route path="/booktable" exact>
-          <Booktable />
-        </Route>
-        <Route path="/edit-bookingmanagement/:id"  component={Editbookings}>
-        </Route>
-   
-      
-        <Route path="/add-cancelbooking/:id" component={Cancelbookingform} >
-        </Route>
-
-       
-        <Route path="/edit-driver/:id" component={DriverUpdate} />
-        <Route path ="/Add-Driver" component={AddDriver}/>
-        <Route path ="/View-Driver" component={DriverDetails}/>
-
-
-        
-        <Route //Setting path for hotels
-          path="/" exact >
-          <HotelHome />
-        </Route>
-
-        <Route path="/add" exact >
-          <AddHotel />
-        </Route>
-
-        <Route path="/edit/:id"  component={EditHotel}>
-        </Route>
-
-        <Route path="/get" exact >
-          <HotelDetails />
-        </Route>
-
-
-
-        <Route path="/my-complaints" exact>
-          <MyComplaints user={user}></MyComplaints>
-        </Route>
-        <Route path="/my-feedbacks" exact>
-          <MyFeedbacks user={user}></MyFeedbacks>
-        </Route>
-
-
         <Redirect to="/index" />
       </Switch>
     </BrowserRouter>
