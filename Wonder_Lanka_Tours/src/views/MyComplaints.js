4fe839158a0f44e9514cb06a48e330b6ca3c02a9
o_weerasinghe <oshanweerasinghe28@gmail.com>
2021-09-17 13:08:08 +0530

    deleted routes and models v1.0

--- Wonder_Lanka_Tours/src/views/MyComplaints.js
+++ Wonder_Lanka_Tours/src/views/MyComplaints.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import{ useHistory } from "react-router-dom"
 // reactstrap components
 
 // core components
@@ -8,14 +11,7 @@ import {
   FormGroup,
   Label,
   Input,
-  Button,
-  Title,
-  Table,
-  TableRow,
-  rows,
-  TableCell,
-  TableHead,
-  TableBody
+  Button
 } from "reactstrap";
 
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
@@ -23,7 +19,8 @@ import IndexHeader from "components/Headers/IndexHeader";
 import DemoFooter from "components/Footers/DemoFooter.js";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- function MyComplaints() {
+toast.configure(); 
+function MyComplaints() {
   //let [complaint, setComplaint] = useState(0)
   document.documentElement.classList.remove("nav-open");
 
@@ -42,7 +39,7 @@ import DemoFooter from "components/Footers/DemoFooter.js";
   const [email, setEmail] = useState("");
   const [contact, setContact] = useState("");
   const [select, setSelect] = useState("");
-  const [text, setText] = useState("");
+  const [text, setComplaint] = useState("");
 
   function sendData(e){
     e.preventDefault();
@@ -63,14 +60,15 @@ import DemoFooter from "components/Footers/DemoFooter.js";
       setEmail("");
       setContact("");
       setSelect("");
-      setText("");
+      setComplaint("");
 
     }).catch((err)=>{
-      alert(err)
+      alert(err);
     })
 
   }
 
+  
   return (
     <>
       <h3>My Complaints</h3>
@@ -125,7 +123,7 @@ import DemoFooter from "components/Footers/DemoFooter.js";
       <FormGroup>
         <Label for="exampleText">Any other custom complaint</Label>
         <Input type="textarea" name="text" id="idText" onChange={(e)=>{
-          setText(e.target.value);
+          setComplaints(e.target.value);
         }}/>
       </FormGroup>
       <Button color="primary" type="submit">
@@ -140,12 +138,15 @@ import DemoFooter from "components/Footers/DemoFooter.js";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
- function MyComplaintsTable () {
-  const [complaints, setComplaints] = useState([]);
+function MyComplaintsTable () {
+  const [complaints, setComplaint] = useState([]);
+  const [searchVal , setSearchVal] = useState("");
+
 
   useEffect(() => {
     function getComplaints() {
       axios.get("http://localhost:8070/MyComplaints").then((res) => {
+        setComplaint(res.data);
         console.log(res);
       }).catch((err) => {
         alert(err.message);
@@ -154,32 +155,98 @@ import DemoFooter from "components/Footers/DemoFooter.js";
     getComplaints();
   },[])
 
+  function complaintDelete(complaint)  {
+    if (
+        window.confirm(
+          "Complaint " + complaint.tourId + " will be removed from the database"
+        )
+    )
+    axios.delete(`http://localhost:8070/itineraries/delete/${complaint._id}`).then((res) =>{
+        console.log(res);
+        
+        toast.error('Complaint Deleted!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
+        
+    }).catch((err) =>{
+        console.log(err);
+        alert(err);
+    })
+  }
+
+  var number = 1;
+
   return (
-    <>
-      <Title>Complaint History</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Tour ID</TableCell>
-            <TableCell>Customer ID</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Complaint</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.tid}>
-              <TableCell>{row.tid}</TableCell>
-              <TableCell>{row.cid}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.complaint}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <DemoFooter />
-    </>
-  )
+
+    <div>
+      <Input placeholder="Search " type="text" 
+        onChange = {(e) =>{
+          setSearchVal(e.target.value);
+      }}/>
+      <div style = {{marginLeft:"20px"}}  className = "tableContainer">
+        <table className = "table table-striped">
+          <thead>
+              <th scope = "col">#</th>
+              <th scope = "col">Tour ID</th>
+              <th scope = "col">Customer ID</th>
+              <th scope = "col">Date</th>
+              <th scope = "col">Complaint</th>
+
+          </thead>
+
+          <tbody>
+              
+              {complaints.filter((val) =>{
+                
+                if(searchVal === ''){
+                  return val;
+                }
+                else if (val.tourId.toLowerCase().includes(searchVal.toLowerCase())){
+                  return val;
+                }
+              
+              }).map((complaint) =>(
+                  
+                  <tr>
+                    <th scope = "row">{number++}</th>
+                    <td>{complaint.tourId}</td>
+                    <td>{complaint.customerID}</td>
+                    <td>{complaint.date}</td>
+                    <td>{complaint.customercomplaint}</td>
+
+
+                    <td><Button color="warning"  style = {{padding: "5px 5px 5px 5px" , width : "80px" , marginBottom : "8px"}}
+                      onClick = {()=>{
+                          history.push(`/edit-complaint/${complaint._id}`);
+                      }}
+                      >Edit</Button>
+
+                      <Button color="danger" style = {{padding: "5px 5px 5px 5px", width : "80px"}}
+                      onClick = {() =>{
+                          
+                          complaintDelete(complaint);
+                      }}
+                    
+                      >Remove</Button>
+                    </td>
+
+                  </tr>
+
+              ))}
+          </tbody>    
+        </table>
+        <DemoFooter />
+      </div>
+    </div>
+  
+
+  );
 }
 
 export {
