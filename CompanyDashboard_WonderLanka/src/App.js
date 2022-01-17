49a65d55c71fb2e607b4301fa5f5a7389c8621b0
SKShehan <shehansilva2013@gmail.com>
2022-01-17 13:41:11 +0530

    Update

--- CompanyDashboard_WonderLanka/src/App.js
+++ CompanyDashboard_WonderLanka/src/App.js
@@ -84,7 +84,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="" render={(props) => <Index {...props} />} />
+        <Route path="/index" render={(props) => <Index {...props} />} />
         <Route
           path="/nucleo-icons"
           render={(props) => <NucleoIcons {...props} />}
